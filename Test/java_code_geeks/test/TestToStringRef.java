599b62ac60d6572065cef1123f42e29da71015ce
neerajsingh <neerajsingh@192.168.1.7>
2015-10-09 08:22:43 -0400

    initial code

---
+++ Test/java_code_geeks/test/TestToStringRef.java
@@ -0,0 +1,26 @@
+package test;
+
+public class TestToStringRef {
+	String name;
+
+	public static void main(String[] args) {
+		TestToStringRef ref1 = new TestToStringRef();
+		TestToStringRef ref2 = new TestToStringRef();
+
+		System.out.println(ref1 + " & " + ref2);
+
+		callMe(ref1, ref2);
+		System.out.println(ref1 + " & " + ref2);
+	}
+
+	//references are passed by value
+	private static void callMe(TestToStringRef... ref) {
+		
+		System.out.println(ref[0] + " & " + ref[1]);
+		ref[0] = ref[1];
+
+		ref[1].name = "new";
+		System.out.println(ref[0] + " & " + ref[1]);
+
+	}
+}
